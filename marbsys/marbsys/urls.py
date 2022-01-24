f264c176cb1dd3dacb01462d91b2122f8459504a
AmrEidAbdelrahman <amreid248@gmail.com>
2022-01-24 15:27:16 +0200

    add commens

--- marbsys/marbsys/urls.py
+++ marbsys/marbsys/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-
+# to handle the static files on developing
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
